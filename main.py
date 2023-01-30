1070b89427921fc279a40b138448098aa74495d5
unknown <bensandlin2@gmail.com>
2023-01-30 10:43:40 -0600

    Completed exercise 1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #Python if else, for loop, and range() Exercises with Solutions
+#Execise 1: Print first 10 natural numbers using while loop
+#Extremely simple. A good warmup.
+counter = 1
+while counter <=10:
+    print(counter)
+    counter += 1
