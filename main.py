3b38ef0aab9934482e962dd82dc006f149bc702b
unknown <bensandlin2@gmail.com>
2023-01-30 10:51:52 -0600

    Completed exercise 2

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ counter = 1
 while counter <=10:
     print(counter)
     counter += 1
+
+#Exercise 2: Print the following pattern
+#They then provide a picture. It shouldn't be too hard.
+row = 5
+for counter1 in range(1, row + 1, 1):
+    for counter2 in range(1, counter1 + 1):
+        print(counter2, end=' ')
+    print("")
